9378d750312b0b1760481141c9198e75c42bc9ef
romaiajr <romaiajr.dev@gmail.com>
2024-08-28 00:28:00 -0300

    :construction: feat: Estrutura base do text-to-speak

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<script setup lang="ts">
+const { setSpeechPreferences } = useConfigStore();
+const i18n = useI18n();
+
+const setupSpeechPreferences = () => {
+  const onVoicesChanged = () => {
+    const voices = speechSynthesis.getVoices();
+    const voice = voices.find((v) => v.lang === i18n.locale.value) || null;
+    setSpeechPreferences({
+      lang: i18n.locale.value,
+      voice,
+    });
+
+    speechSynthesis.removeEventListener('voiceschanged', onVoicesChanged);
+  };
+
+  speechSynthesis.addEventListener('voiceschanged', onVoicesChanged);
+
+  if (speechSynthesis.getVoices().length) {
+    onVoicesChanged();
+  }
+};
+
+onMounted(() => {
+  setupSpeechPreferences();
+  navigateTo(Routes.WELCOME);
+});
+</script>
