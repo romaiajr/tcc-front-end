26f9e1ffd07bfed8ed5bab35f192ecacbd6fd279
RobertoToodoo <roberto.maia@toodoo.com.br>
2024-10-15 09:35:46 -0300

    fix: forgotten files of last commit

--- src/interfaces/pdv-menu.ts
+++ src/interfaces/pdv-menu.ts
@@ -8,6 +8,7 @@ export interface MenuItem {
   label: string;
   action?: () => void;
   infoText?: string;
+  complementText?: string;
 }
 
 export interface Menu {
@@ -19,10 +20,13 @@ export enum DerFlowEnum {
   DEFAULT,
   ENTITIES,
   ENTITY_OPTIONS,
+  DELETE_ENTITY,
   ATTRS,
   ATTR_OPTIONS,
+  DELETE_ATTR,
   RELATIONSHIPS,
   RELATIONSHIP_OPTIONS,
+  DELETE_RELATIONSHIP,
   NEW_ENTITY,
   NEW_ATTR,
   NEW_RELATIONSHIP,
@@ -35,10 +39,13 @@ export const previousDerMenuMapping: Record<
   [DerFlowEnum.DEFAULT]: undefined,
   [DerFlowEnum.ENTITIES]: DerFlowEnum.DEFAULT,
   [DerFlowEnum.ENTITY_OPTIONS]: DerFlowEnum.ENTITIES,
+  [DerFlowEnum.DELETE_ENTITY]: DerFlowEnum.ENTITY_OPTIONS,
   [DerFlowEnum.ATTRS]: DerFlowEnum.ENTITY_OPTIONS,
   [DerFlowEnum.ATTR_OPTIONS]: DerFlowEnum.ATTRS,
+  [DerFlowEnum.DELETE_ATTR]: DerFlowEnum.ATTR_OPTIONS,
   [DerFlowEnum.RELATIONSHIPS]: DerFlowEnum.DEFAULT,
   [DerFlowEnum.RELATIONSHIP_OPTIONS]: DerFlowEnum.RELATIONSHIPS,
+  [DerFlowEnum.DELETE_RELATIONSHIP]: DerFlowEnum.RELATIONSHIP_OPTIONS,
   [DerFlowEnum.NEW_ENTITY]: DerFlowEnum.DEFAULT,
   [DerFlowEnum.NEW_ATTR]: DerFlowEnum.ENTITY_OPTIONS,
   [DerFlowEnum.NEW_RELATIONSHIP]: DerFlowEnum.DEFAULT,
